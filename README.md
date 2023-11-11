6edded9ca8dbbc10c74424f478a8f1442fff20a3
Jack Harner <jackharner3@gmail.com>
2023-11-10 21:59:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# About this App
+
+This is a complex 18 component, 400+ line single page React app that allows users to search a database of movies, rate, and add them to their list
+
+## Technical components inovolved
+
+- React props, state, custom hooks, refs, effects
+- localstorage
+- API calls
+
+## Run this project
+
+1. Install Node.js version 18
+
+2. In the project directory run `npm install` to install the dependencies
+
+3. Run `npm start` to start the app on localhost
+
+4. Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ src/App.js
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import { useMovies } from "./components/useMovies";
+import { useLocalStorageState } from "./components/useLocalStorageState";
+import { Loader } from "./components/Loader";
+import { ErrorMessage } from "./components/ErrorMessage";
+import { NavBar } from "./components/NavBar";
+import { Search } from "./components/Search";
+import { Numresults } from "./components/Numresults";
+import { Main } from "./components/Main";
+import { Box } from "./components/Box";
+import { MovieList } from "./components/MovieList";
+import { MovieDetails } from "./components/MovieDetails";
+import { WatchedSummary } from "./components/WatchedSummary";
+import { WatchedMoviesList } from "./components/WatchedMoviesList";
+
+export const KEY = "bf677b32"; // API key is only stored here bcause this is a free API
+
+export const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+  const { movies, isLoading, error } = useMovies(query);
+
+  const [watched, setWatched] = useLocalStorageState([], "watched");
+
+  function handleSelectMovie(id) {
+    // console.log("method entered with id ", id);
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
+
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
+
+  useEffect(
+    function () {
+      // console.log("D");
+    },
+    [query]
+  );
+
+  function handleAddWatch(movie) {
+    setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
+  }
+
+  function handleDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+  }
+
+  return (
+    <>
+      <NavBar>
+        <Search query={query} setQuery={setQuery} />
+        <Numresults movies={movies} />
+      </NavBar>
+      <Main>
+        <Box>
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {isLoading && <Loader />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+          )}
+          {error && <ErrorMessage message={error} />}
+        </Box>
+
+        <Box>
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+              onAddWatched={handleAddWatch}
+              watched={watched}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList
+                watched={watched}
+                onDeleteWatched={handleDeleteWatched}
+              />
+            </>
+          )}
+        </Box>
+      </Main>
+    </>
+  );
+}
